fb808e5a747df077e2925fa3a824a1823307049a
mintesnotyared <mintesnotyared5@gmail.com>
2025-10-07 18:59:34 +0300

    smartswitch.py

--- smartswitch.py
+++ smartswitch.py
@@ -4,7 +4,7 @@ app = Flask(name)
 
 @app.route('/')
 def home():
-    return "Smart Switch Cloud is Live!"
+    return "Minte Smart Switch Cloud is Live!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
